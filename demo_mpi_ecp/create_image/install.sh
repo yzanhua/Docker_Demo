ec41f62a786e900ad585dbf840b67f8d858a0a70
Zanhua Huang <yzanhua@gmail.com>
2021-11-17 19:02:14 -0600

    ecp use

--- demo_mpi_ecp/create_image/install.sh
+++ demo_mpi_ecp/create_image/install.sh
@@ -1,11 +1,3 @@
-# run a new container using image gcc version 9.4
-# you can choose a different version.
-# supported version: https://hub.docker.com/_/gcc?tab=tags
-
-# numa.h needed by mpich
-apt-get -qq update
-apt-get install -qqy libnuma-dev
-
 # install mpich
 wget http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
 tar xf mpich-3.4.2.tar.gz
@@ -14,23 +6,21 @@ rm mpich-3.4.2.tar.gz
 mkdir /mpich
 # build folder
 mkdir /mpich-build
-cd mpich-build
-/mpich-3.4.2/configure -prefix=/mpich --with-device=ch4:ucx --disable-fortan 2>&1 | tee c.txt
+cd /mpich-build
+/mpich-3.4.2/configure -prefix=/mpich --with-device=ch4:ofi --disable-fortan 2>&1 | tee c.txt
 make 2>&1 | tee m.txt
 make install 2>&1 | tee mi.txt 
 echo 'export PATH="/mpich/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 cd /
 
-# install python3 
-apt-get -qq update
-apt-get install -qqy python3
-
+# python3 is already installed in gcc:9.4 
 # install pip
-apt-get install -qqy python3-pip
+curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+rm get-pip.py
 
 # install mpi4py
-python3 -m pip install --upgrade pip
 python3 -m pip install mpi4py
 
 rm -rf /mpich-build
