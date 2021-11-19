8d284d17991671c613f29618410ea2d4571b7f49
wkliao <wkliao@users.noreply.github.com>
2021-11-19 10:21:56 -0600

    silence outputs

--- demo_mpi_ecp/create_image/install.sh
+++ demo_mpi_ecp/create_image/install.sh
@@ -1,5 +1,5 @@
 # install mpich
-wget http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
+wget --no-verbose http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
 tar xf mpich-3.4.2.tar.gz
 rm mpich-3.4.2.tar.gz
 # installtion folder
@@ -7,9 +7,9 @@ mkdir /mpich
 # build folder
 mkdir /mpich-build
 cd /mpich-build
-/mpich-3.4.2/configure -prefix=/mpich --with-device=ch4:ofi --disable-fortran 2>&1 | tee c.txt
-make 2>&1 | tee m.txt
-make install 2>&1 | tee mi.txt 
+/mpich-3.4.2/configure --prefix=/mpich --with-device=ch4:ofi --disable-fortran --silence 2>&1 | tee c.txt
+make -s LIBTOOLFLAGS=--silent -V -j4 2>&1 | tee m.txt
+make -s install 2>&1 | tee mi.txt
 echo 'export PATH="/mpich/bin:$PATH"' >> ~/.bashrc
 export PATH="/mpich/bin:$PATH"
 cd /
@@ -19,7 +19,7 @@ cd /
 pip install --upgrade pip
 
 # install mpi4py
-pip install mpi4py
+pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
 
 rm -rf /mpich-build
 rm -rf /mpich-3.4.2
@@ -27,3 +27,4 @@ rm -rf /mpich-3.4.2
 mkdir /mpich-demo
 mkdir /mpich-demo/c
 mkdir /mpich-demo/py
+
